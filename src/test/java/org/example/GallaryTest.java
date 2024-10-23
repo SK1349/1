211d18732d8d067acdbb2786266dcbf5067186e3
SK <boo.ochevidno@gmail.com>
2024-10-23 10:45:12 +0300

    up

---
+++ src/test/java/org/example/GallaryTest.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+
+public class GallaryTest {
+    @Test
+    public void add() {
+        String data = "e";
+        Gallery<String> received = new Gallery<>();
+        received.add(data,0);
+
+        Gallery<String> requiritur = new Gallery<>();
+
+    }
+}
